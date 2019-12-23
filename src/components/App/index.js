d757724d6a597ea4164844acad7d2c760e2e20fa
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2019-12-23 23:56:48 +0300

    upload the project

---
+++ src/components/App/index.js
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { lazy, Suspense, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+
+import Footer from '@components/Footer';
+import Hero from '@components/Hero';
+import { listenWithRmanifest } from '../../utils';
+import style from './index.module.css';
+
+const Home = lazy(() => import(/* webpackChunkName: "home" */ '@pages/Home'));
+const About = lazy(() => import(/* webpackChunkName: "about" */ '@pages/About'));
+const Article = lazy(() => import(/* webpackChunkName: "article" */ '@pages/Article'));
+const Blog = lazy(() => import(/* webpackChunkName: "blog" */ '@pages/Blog'));
+
+const withQuicklink = Component => {
+	return () => {
+		useEffect(() => {
+			const target = document.querySelector('#route-component');
+			const options = {
+				root: null,
+				rootMargin: '0px'
+			};
+			const callback = entries => {
+				const intersectionRatio = entries[0].intersectionRatio;
+				console.log('ray : ***** [App withQuicklink callback] intersectionRatio => ', intersectionRatio);
+				if (intersectionRatio > 0) {
+					console.log('ray : ***** [App withQuicklink callback] we call quicklink as intersectionRatio is ', intersectionRatio, ', which is greater than zero');
+					listenWithRmanifest();
+				}
+			};
+			const observer = new IntersectionObserver(callback, options);
+			observer.observe(target);
+		}, []);
+		
+		return (
+			<div id='route-component'>
+				<Component />
+			</div>
+		);
+	};
+};
+
+const App = () => (
+	<div className={style.app}>
+		<Hero />
+		<main className={style.wrapper}>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Route path="/" exact component={withQuicklink(Home)} />
+				<Route path="/blog" exact component={withQuicklink(Blog)} />
+				<Route path="/blog/:title" component={withQuicklink(Article)} />
+				<Route path="/about" exact component={withQuicklink(About)} />
+			</Suspense>
+		</main>
+		<Footer />
+	</div>
+);
+
+export default App;
---
+++ src/utils/index.js
@@ -0,0 +1,26 @@
+
+import { listen } from './quicklink/index.mjs';
+
+const listenWithRmanifest = async () => {
+  if (!window._rmanifest_) {
+    const response = await fetch('/rmanifest.json');
+    window._rmanifest_ = await response.json();
+  }
+
+  const rmanifest = window._rmanifest_;
+  let chunks = {};
+  for (const route in rmanifest) {
+    if (route !== '*' && rmanifest.hasOwnProperty(route)) {
+      const chunksForRoute = rmanifest[route];
+      const chunkURLs = chunksForRoute.map(chunkForRoute => chunkForRoute.href);
+      chunks = {...chunks, [route]: chunkURLs};
+    }
+  }
+  
+  console.log('ray : ***** chunks => ', chunks);
+  listen({chunks});
+};
+
+export {
+  listenWithRmanifest
+};
