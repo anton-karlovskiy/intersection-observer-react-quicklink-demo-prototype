b1d5e54c6cee3b497affa6bd413e3c8f161f659a
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-04-13 23:07:43 +0300

    chore: indents cleaned up

--- src/lib/quicklink.js
+++ src/lib/quicklink.js
@@ -23,25 +23,22 @@ const useIntersect = ({ root = null, rootMargin, threshold = 0 } = {}) => {
   const [node, setNode] = useState(null);
   const observer = useRef(null);
 
-  useEffect(
-    () => {
-      if (observer.current) observer.current.disconnect();
-      observer.current = new window.IntersectionObserver(
-        ([entry]) => updateEntry(entry),
-        {
-          root,
-          rootMargin,
-          threshold
-        }
-      );
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect();
+    observer.current = new window.IntersectionObserver(
+      ([entry]) => updateEntry(entry),
+      {
+        root,
+        rootMargin,
+        threshold
+      }
+    );
 
-      const { current: currentObserver } = observer;
-      if (node) currentObserver.observe(node);
+    const { current: currentObserver } = observer;
+    if (node) currentObserver.observe(node);
 
-      return () => currentObserver.disconnect();
-    },
-    [node, root, rootMargin, threshold]
-  );
+    return () => currentObserver.disconnect();
+  }, [node, root, rootMargin, threshold]);
 
   return [setNode, entry];
 };
